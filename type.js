e451690f91c7b4a379058917b0ddf975763e95e4
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-27 20:35:02 +0600

    first commit

---
+++ type.js
@@ -0,0 +1,7 @@
+// strongly typed language
+//  int a = 5;
+//  string b = 'alim halim kalim';
+//  bool c = True;
+//  object student = { name: 'noya daman', id: 55}
+//  int[] numbers = [12, 45, 78]
+//  string[] friends = ['abul', 'babul']
