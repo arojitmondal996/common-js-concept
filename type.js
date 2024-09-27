2f50f9159e7697e65b9b9c30e5e4205ccf2ebaf7
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-27 20:54:29 +0600

    premitive and non primitive type in javascript

--- type.js
+++ type.js
@@ -5,3 +5,25 @@
 //  object student = { name: 'noya daman', id: 55}
 //  int[] numbers = [12, 45, 78]
 //  string[] friends = ['abul', 'babul']
+
+
+// Javascript dynamic type language
+// primitive type
+const a = 5;
+const b = 'Sagor';
+
+// non-primitive
+const ages = [45, 65, 48];
+const student = {id: 23, class: 7}
+console.log(typeof a, typeof b, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = {job: 'web developer'}
+let q = p;
+q = {job: 'Backend end'}
+console.log(p, q);
